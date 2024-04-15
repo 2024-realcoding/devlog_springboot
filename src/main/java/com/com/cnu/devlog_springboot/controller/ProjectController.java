58d8afc61a69e870ecf7c08f43c4fca289f289b7
RAKKUNN <njkat0108@gmail.com>
2024-04-15 22:53:53 +0900

    project_implementation

---
+++ src/main/java/com/com/cnu/devlog_springboot/controller/ProjectController.java
@@ -0,0 +1,51 @@
+package com.com.cnu.devlog_springboot.controller;
+
+import com.com.cnu.devlog_springboot.model.Project;
+import com.com.cnu.devlog_springboot.model.request.ProjectRequest;
+import com.com.cnu.devlog_springboot.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/projects")
+@RequiredArgsConstructor
+public class ProjectController {
+    private final ProjectService projectService;
+
+    // GET /projects
+    @GetMapping
+    public ResponseEntity<List<Project>> getProjects() {
+        return ResponseEntity.ok(projectService.getProjects());
+    }
+
+    // GET /projects/{projectId}
+    @GetMapping("{projectId}")
+    public ResponseEntity<Project> getProject(@PathVariable("projectId") Integer projectId) {
+        return ResponseEntity.ok(projectService.getProject(projectId));
+    }
+
+    // POST /projects
+    @PostMapping
+    public ResponseEntity<Project> createProject(@RequestBody ProjectRequest projectRequest) {
+        return ResponseEntity.ok(projectService.createProject(projectRequest));
+    }
+
+    // PUT /projects/{projectId}
+    @PutMapping("{projectId}")
+    public ResponseEntity<Project> updateProject(
+            @PathVariable("projectId") Integer projectId,
+            @RequestBody ProjectRequest projectRequest
+    ) {
+        return ResponseEntity.ok(projectService.updateProject(projectId, projectRequest));
+    }
+
+    // DELETE /projects/{projectId}
+    @DeleteMapping("{projectId}")
+    public ResponseEntity<Void> deleteProject(@PathVariable("projectId") Integer projectId) {
+        projectService.deleteProject(projectId);
+        return ResponseEntity.noContent().build();
+    }
+}
