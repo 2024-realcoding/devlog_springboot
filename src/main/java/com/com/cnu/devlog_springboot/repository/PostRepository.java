7ef6a170691810ee265ed130180a4fac3bf0c2e4
ChanyeongCho <jcyart0721@gmail.com>
2024-04-29 04:52:05 +0900

    real coding hw

--- src/main/java/com/com/cnu/devlog_springboot/repository/PostRepository.java
+++ src/main/java/com/com/cnu/devlog_springboot/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.com.cnu.devlog_springboot.repository;
 
 import com.com.cnu.devlog_springboot.model.Post;
 import com.com.cnu.devlog_springboot.type.Tag;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/com/cnu/devlog_springboot/type/ErrorCode.java
+++ src/main/java/com/com/cnu/devlog_springboot/type/ErrorCode.java
@@ -5,12 +5,16 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-    //4xx
-
-    POST_NOT_FOUND(HttpStatus.NOT_FOUND,
+    POST_NOT_FOUND(
+            HttpStatus.NOT_FOUND,
             4000,
-            "해당 게시글을 못 찾겠다."
-    );
+            "해당 게시글을 찾을 수 없습니다."),
+
+    PROJECT_NOT_FOUND(
+            HttpStatus.NOT_FOUND,
+            4001,
+            "해당 프로젝트를 찾을 수 없습니다.")
+    ;
 
     private final HttpStatus httpStatus;
     private final Integer errorCode;
