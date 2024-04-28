655c2ce44d8e8d7eb9c774576855901c434b1092
ChanyeongCho <jcyart0721@gmail.com>
2024-04-29 04:02:41 +0900

    real coding hw

---
+++ src/main/java/com/com/cnu/devlog_springboot/type/ErrorCode.java
@@ -0,0 +1,24 @@
+package com.com.cnu.devlog_springboot.type;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+    //4xx
+
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND,
+            4000,
+            "해당 게시글을 못 찾겠다."
+    );
+
+    private final HttpStatus httpStatus;
+    private final Integer errorCode;
+    private final String description;
+
+    ErrorCode(HttpStatus httpStatus, Integer errorCode, String description) {
+        this.httpStatus = httpStatus;
+        this.errorCode = errorCode;
+        this.description = description;
+    }
+}
