bc7d090a11acce966f091196e4e396d6a0dfe260
ensalada-de-pechuga <villop020403@o.cnu.ac.kr>
2024-04-28 18:07:21 +0900

    commit

--- src/main/java/com/com/cnu/devlog_springboot/repository/ProjectRepository.java
+++ src/main/java/com/com/cnu/devlog_springboot/repository/ProjectRepository.java
@@ -1,2 +1,8 @@
-package com.com.cnu.devlog_springboot.repository;public interface ProjectRepository {
-}
+package com.com.cnu.devlog_springboot.repository;
+
+import com.com.cnu.devlog_springboot.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Integer> {}
