927939adafa86ba3d9a1a9f858f0a18236f25a3a
Wantorich <hyojae5945@gmail.com>
2024-04-30 08:56:37 +0900

    post -> project로 변경

---
+++ src/main/java/com/com/cnu/devlog_springboot/service/ProjectService.java
@@ -0,0 +1,60 @@
+package com.com.cnu.devlog_springboot.service;
+
+import com.com.cnu.devlog_springboot.exception.DevlogException;
+import com.com.cnu.devlog_springboot.model.Project;
+import com.com.cnu.devlog_springboot.model.request.ProjectRequest;
+import com.com.cnu.devlog_springboot.repository.ProjectRepository;
+import com.com.cnu.devlog_springboot.type.ErrorCode;
+import com.com.cnu.devlog_springboot.type.Tag;
+import io.micrometer.common.lang.Nullable;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProjectService {
+    private final ProjectRepository projectRepository;
+
+    public List<Project> getProjects() {
+//        if (tag != null) {
+//            return projectRepository.findAllByTag(tag);
+//        }
+        return projectRepository.findAll();
+    }
+
+    public Project createProject(ProjectRequest projectRequest) {
+        return projectRepository.save(new Project(
+                null,
+                projectRequest.title(),
+                projectRequest.contents(),
+                projectRequest.summary(),
+                projectRequest.startDate(),
+                projectRequest.endDate()
+        ));
+    }
+
+    public Project updateProject(Integer projectId, ProjectRequest projectRequest) {
+        return projectRepository.findById(projectId)
+                .map(project -> {
+                            project.setTitle(projectRequest.title());
+                            project.setContents(projectRequest.contents());
+                            project.setSummary(projectRequest.summary());
+                            project.setStartDate(projectRequest.startDate());
+                            project.setEndDate(projectRequest.endDate());
+                            return projectRepository.save(project);
+                        })
+                .orElse(null);
+    }
+
+    public Project getProject(Integer projectId) {
+        return projectRepository.findById(projectId)
+                .orElseThrow(() -> new DevlogException(ErrorCode.POST_NOT_FOUND));
+    }
+
+    public void deleteProject(Integer projectId) {
+        projectRepository.findById(projectId)
+                .ifPresent(projectRepository::delete);
+    }
+}
