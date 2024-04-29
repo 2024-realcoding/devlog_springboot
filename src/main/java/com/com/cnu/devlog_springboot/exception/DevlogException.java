28559d2d9458cfc95e23bb9c3962460c16ff1852
Nope777 <suminwow1@gmail.com>
2024-04-29 23:28:18 +0900

    Project code complete

---
+++ src/main/java/com/com/cnu/devlog_springboot/exception/DevlogException.java
@@ -0,0 +1,11 @@
+package com.com.cnu.devlog_springboot.exception;
+
+import com.com.cnu.devlog_springboot.type.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DevlogException extends RuntimeException {
+    private final ErrorCode errorCode;
+}
