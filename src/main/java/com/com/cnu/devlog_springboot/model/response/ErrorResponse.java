c511ec6ea921f02ea7a7730e0ab793fbd33781d1
hekengi <118739381+hekengi@users.noreply.github.com>
2024-05-03 17:46:09 +0900

    Delete src/main/java/com/com/cnu/devlog_springboot/model/response directory

--- src/main/java/com/com/cnu/devlog_springboot/model/response/ErrorResponse.java
+++
@@ -1,9 +0,0 @@
-package com.com.cnu.devlog_springboot.model.response;
-
-public record ErrorResponse(
-        String title,
-        Integer status,
-        Integer code,
-        String instance
-) {
-}
