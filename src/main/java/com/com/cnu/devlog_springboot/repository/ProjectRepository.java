c003d8bb6522cf8eb6e6517c9745743f438d4a15
minji <qkralswl6125@naver.com>
2024-05-05 22:26:26 +0900

    feat: project 과제 기능 구현

---
+++ src/main/java/com/com/cnu/devlog_springboot/repository/ProjectRepository.java
@@ -0,0 +1,9 @@
+package com.com.cnu.devlog_springboot.repository;
+
+import com.com.cnu.devlog_springboot.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Integer> {
+}
