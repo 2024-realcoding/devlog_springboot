4ba010042b842a7b9c243f80c10d80d28ab401ce
iyeaaa <iyeaaa@icloud.com>
2024-05-05 09:40:13 +0900

    implement CRUD

--- src/main/java/com/com/cnu/devlog_springboot/repository/PostRepository.java
+++ src/main/java/com/com/cnu/devlog_springboot/repository/PostRepository.java
@@ -1,6 +1,8 @@
 package com.com.cnu.devlog_springboot.repository;
 
 import com.com.cnu.devlog_springboot.model.Post;
+import com.com.cnu.devlog_springboot.type.Tag;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,5 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
 //    List<Post> findAll();
 //    Optional<Post> findById(Integer postId);
 //    void delete(Post post);
+    List<Post> findAllByTag(Tag tag);
 }
