5aa0f18e83b7704082a1237ccb2eb437c2a2ee4e
U-S-jun <157643778+U-S-jun@users.noreply.github.com>
2024-05-06 16:13:43 +0900

    Update PostRepository.java

--- src/main/java/com/com/cnu/devlog_springboot/repository/PostRepository.java
+++ src/main/java/com/com/cnu/devlog_springboot/repository/PostRepository.java
@@ -4,6 +4,8 @@ import com.com.cnu.devlog_springboot.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import com.com.cnu.devlog_springboot.type.Tag;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
     List<Post> findAllByTag(Tag tag);
