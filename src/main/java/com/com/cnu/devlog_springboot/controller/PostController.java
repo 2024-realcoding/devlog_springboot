e4e1e8a1d9795f4f6cea0ee74455263d7c044b79
juhabae <141327091+juhabae@users.noreply.github.com>
2024-05-06 21:39:22 +0900

    Update PostController.java

--- src/main/java/com/com/cnu/devlog_springboot/controller/PostController.java
+++ src/main/java/com/com/cnu/devlog_springboot/controller/PostController.java
@@ -3,10 +3,10 @@ package com.com.cnu.devlog_springboot.controller;
 import com.com.cnu.devlog_springboot.model.Post;
 import com.com.cnu.devlog_springboot.model.request.PostRequest;
 import com.com.cnu.devlog_springboot.service.PostService;
-import com.com.cnu.devlog_springboot.type.Tag;
+import com.com.cnu.devlog_springboot.type.Tag; // Tag import
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.Nullable;
+import org.springframework.lang.Nullable; // Nullable import
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,27 +19,27 @@ public class PostController {
 
     // GET /posts
     @GetMapping
-    public ResponseEntity<List<Post>> getPosts(@RequestParam @Nullable Tag tag) {
+    public ResponseEntity<List<Post>> getPosts(@RequestParam @Nullable Tag tag) { // Tag 파라미터 추가
         return ResponseEntity.ok(postService.getPosts(tag));
     }
 
     // GET /posts/{postId}
     @GetMapping("{postId}")
-    public ResponseEntity<Post> getPost(@PathVariable("postId")Integer postId) {
+    public ResponseEntity<Post> getPost(@PathVariable("postId") Integer postId) {
         return ResponseEntity.ok(postService.getPost(postId));
     }
 
     // POST /posts
     @PostMapping
     public ResponseEntity<Post> createPost(@RequestBody PostRequest postRequest) {
-        return ResponseEntity.ok(postService.creatPost(postRequest));
+        return ResponseEntity.ok(postService.createPost(postRequest)); // 오타 수정: creatPost -> createPost
     }
 
     // PUT /posts/{postId}
     // ex. localhost:8080/posts/3
     @PutMapping("{postId}")
     public ResponseEntity<Post> updatePost(
-            @PathVariable("postId")Integer postId,
+            @PathVariable("postId") Integer postId,
             @RequestBody PostRequest postRequest
     ) {
         return ResponseEntity.ok(postService.updatePost(postId, postRequest));
