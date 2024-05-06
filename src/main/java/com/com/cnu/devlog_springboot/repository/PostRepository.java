c16260d8c123d46b323e72d7b9489c2d28b3be9d
hongdyu01 <144324356+hongdyu01@users.noreply.github.com>
2024-05-06 15:13:08 +0900

    Update PostRepository.java

--- src/main/java/com/com/cnu/devlog_springboot/repository/PostRepository.java
+++ src/main/java/com/com/cnu/devlog_springboot/repository/PostRepository.java
@@ -4,7 +4,7 @@ import com.com.cnu.devlog_springboot.model.Post;
 import com.com.cnu.devlog_springboot.type.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
 //    Post save(Post post);
