b7c4a10517a2281048be306856c25b0bb751cf5f
Nope777 <suminwow1@gmail.com>
2024-05-06 21:31:53 +0900

    commit test

--- src/main/java/com/com/cnu/devlog_springboot/service/ProjectService.java
+++ src/main/java/com/com/cnu/devlog_springboot/service/ProjectService.java
@@ -52,7 +52,5 @@ public class ProjectService {
         projectRepository.findById(projectId)
                 .ifPresentOrElse(projectRepository::delete,
                         () -> {throw new DevlogException(ErrorCode.POST_NOT_FOUND);});
-
-
     }
 }
