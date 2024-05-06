a95e6c76f975895056a0d5d6582e1fb8d45e22b3
cussle <cussle@kakao.com>
2024-05-06 22:49:58 +0900

    minor fix

--- src/main/java/com/com/cnu/devlog_springboot/DevlogSpringbootApplication.java
+++ src/main/java/com/com/cnu/devlog_springboot/DevlogSpringbootApplication.java
@@ -1,9 +1,13 @@
 package com.com.cnu.devlog_springboot;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@RequestMapping("/posts")
+@RequiredArgsConstructor
 public class DevlogSpringbootApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/com/cnu/devlog_springboot/service/ProjectService.java
+++ src/main/java/com/com/cnu/devlog_springboot/service/ProjectService.java
@@ -1,2 +1,53 @@
-package com.com.cnu.devlog_springboot.service;public class ProjectService {
+package com.com.cnu.devlog_springboot.service;
+
+import com.com.cnu.devlog_springboot.model.Project;
+import com.com.cnu.devlog_springboot.model.request.ProjectRequest;
+import com.com.cnu.devlog_springboot.repository.ProjectRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProjectService {
+    private final ProjectRepository projectRepository;
+
+    public Project createProject(ProjectRequest projectRequest) {
+        return projectRepository.save(new Project(
+                null,
+                projectRequest.title(),
+                projectRequest.summary(),
+                projectRequest.contents(),
+                projectRequest.startDate(),
+                projectRequest.endDate()
+        ));
+    }
+
+    public List<Project> getProjects() {
+        return projectRepository.findAll();
+    }
+
+    public Project getProject(Integer projectId) {
+        return projectRepository.findById(projectId)
+                .orElse(null);
+    }
+
+    public Project updateProject(Integer projectId, ProjectRequest projectRequest) {
+        return projectRepository.findById(projectId)
+                .map(project -> {
+                    project.setTitle(projectRequest.title());
+                    project.setContents(projectRequest.contents());
+                    project.setSummary(projectRequest.summary());
+                    project.setStartDate(projectRequest.startDate());
+                    project.setEndDate((projectRequest.endDate()));
+                    return projectRepository.save(project);
+                })
+                .orElse(null);
+    }
+
+    public void deleteProject(Integer projectId) {
+        projectRepository.findById(projectId)
+                .ifPresent(projectRepository::delete);
+    }
 }
