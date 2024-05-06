8968d0a132836e80037fe3e358d2b56789e6c7cd
juhabae <qowngk0729@naver.com>
2024-05-06 20:08:48 +0900

    CURD구현

--- src/main/java/com/com/cnu/devlog_springboot/controller/PostController.java
+++ src/main/java/com/com/cnu/devlog_springboot/controller/PostController.java
@@ -6,14 +6,7 @@ import com.com.cnu.devlog_springboot.service.ProjectService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/projects")
@@ -39,7 +32,7 @@ public class ProjectController {
 
     @PutMapping("{projectId}")
     public ResponseEntity<Project> updateProject(
-            @PathVariable("projectId")Integer projectId,
+            @PathVariable("projectId") Integer projectId,
             @RequestBody ProjectRequest projectRequest
     ) {
         return ResponseEntity.ok(projectService.updateProject(projectId, projectRequest));
