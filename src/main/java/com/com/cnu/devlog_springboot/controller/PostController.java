d20f09ccfb8f1bf043e5647d764a33304ec080c7
[Github <[Github>
2024-05-06 15:54:04 +0900

    project

--- src/main/java/com/com/cnu/devlog_springboot/controller/PostController.java
+++ src/main/java/com/com/cnu/devlog_springboot/controller/PostController.java
@@ -3,6 +3,8 @@ package com.com.cnu.devlog_springboot.controller;
 import com.com.cnu.devlog_springboot.model.Post;
 import com.com.cnu.devlog_springboot.model.request.PostRequest;
 import com.com.cnu.devlog_springboot.service.PostService;
+import com.com.cnu.devlog_springboot.type.Tag;
+import io.micrometer.common.lang.Nullable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,34 +19,33 @@ public class PostController {
 
     // GET /posts
     @GetMapping
-    public ResponseEntity<List<Post>> getPosts() {
-        return ResponseEntity.ok(postService.getPosts());
+    public ResponseEntity<List<Post>> getPosts(@RequestParam("tag") @Nullable Tag tag) {
+        return ResponseEntity.ok(postService.getPosts(tag));
     }
 
     // GET /posts/{postId}
-    @GetMapping("{postId}")
-    public ResponseEntity<Post> getPost(@PathVariable("postId")Integer postId) {
+    @GetMapping("/{postId}")
+    public ResponseEntity<Post> getPost(@PathVariable("postId") Integer postId) {
         return ResponseEntity.ok(postService.getPost(postId));
     }
 
     // POST /posts
     @PostMapping
     public ResponseEntity<Post> createPost(@RequestBody PostRequest postRequest) {
-        return ResponseEntity.ok(postService.creatPost(postRequest));
+        return ResponseEntity.ok(postService.createPost(postRequest));
     }
 
     // PUT /posts/{postId}
-    // ex. localhost:8080/posts/3
-    @PutMapping("{postId}")
+    @PutMapping("/{postId}")
     public ResponseEntity<Post> updatePost(
-            @PathVariable("postId")Integer postId,
+            @PathVariable("postId") Integer postId,
             @RequestBody PostRequest postRequest
     ) {
         return ResponseEntity.ok(postService.updatePost(postId, postRequest));
     }
 
     // DELETE /posts/{postId}
-    @DeleteMapping("{postId}")
+    @DeleteMapping("/{postId}")
     public ResponseEntity<Void> deletePost(@PathVariable("postId") Integer postId) {
         postService.deletePost(postId);
         return ResponseEntity.noContent().build();
---
+++ src/main/java/com/com/cnu/devlog_springboot/type/ErrorCode.java
@@ -0,0 +1,30 @@
+package com.com.cnu.devlog_springboot.type;
+
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+    //4xx
+
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND,
+            4000,
+            "해당 게시글을 찾을 수 없음."
+    ),
+    PROJECT_NOT_FOUND(
+            HttpStatus.NOT_FOUND,
+            4001,
+            "해당 프로젝트를 찾을 수 없음.")
+    ;
+
+    private final HttpStatus httpStatus;
+    private final Integer errorCode;
+    private final String description;
+
+    ErrorCode(HttpStatus httpStatus, Integer errorCode, String description) {
+        this.httpStatus = httpStatus;
+        this.errorCode = errorCode;
+        this.description = description;
+    }
+}
