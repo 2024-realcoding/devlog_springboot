5e15fad57c66c1ddebc5e02f966d08fe32af4dac
user <hellolego2@naver.com>
2024-05-06 13:53:59 +0900

    project 과제

--- src/main/java/com/com/cnu/devlog_springboot/model/Project.java
+++ src/main/java/com/com/cnu/devlog_springboot/model/Project.java
@@ -1,12 +1,13 @@
-package com.com.cnu.devlog_springboot.model;
-
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
 import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Setter
@@ -14,6 +15,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
 public class Project {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
@@ -22,4 +24,24 @@ public class Project {
     String contents;
     LocalDate startDate;
     LocalDate endDate;
+    private Integer id;
+    private String title;
+    private String summary;
+    private String contents;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public void update(
+            String title,
+            String summary,
+            String contents,
+            LocalDate startDate,
+            LocalDate endDate
+    ) {
+        this.title = title;
+        this.summary = summary;
+        this.contents = contents;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
 }
