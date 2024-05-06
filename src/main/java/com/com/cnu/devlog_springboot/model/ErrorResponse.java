5acfa1048a34c2e1cb84e9605cc541e966a1d10d
unknown <cyh1443@gmail.com>
2024-05-06 15:48:17 +0900

    Springboot

---
+++ src/main/java/com/com/cnu/devlog_springboot/model/ErrorResponse.java
@@ -0,0 +1,26 @@
+package com.com.cnu.devlog_springboot.model;
+
+public record ErrorResponse(String title, Integer status, Integer code, String instance) {
+    public ErrorResponse(String title, Integer status, Integer code, String instance) {
+        this.title = title;
+        this.status = status;
+        this.code = code;
+        this.instance = instance;
+    }
+
+    public String title() {
+        return this.title;
+    }
+
+    public Integer status() {
+        return this.status;
+    }
+
+    public Integer code() {
+        return this.code;
+    }
+
+    public String instance() {
+        return this.instance;
+    }
+}
