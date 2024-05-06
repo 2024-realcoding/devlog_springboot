5d08e026f632af78b5a2fb32f37c2e5fd73cd94b
cussle <cussle@kakao.com>
2024-05-06 23:14:42 +0900

    minor fix

--- src/main/java/com/com/cnu/devlog_springboot/DevlogSpringbootApplication.java
+++ src/main/java/com/com/cnu/devlog_springboot/DevlogSpringbootApplication.java
@@ -1,13 +1,9 @@
 package com.com.cnu.devlog_springboot;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@RestController
-@RequestMapping("/posts")
-@RequiredArgsConstructor
+@SpringBootApplication
 public class DevlogSpringbootApplication {
 
     public static void main(String[] args) {
