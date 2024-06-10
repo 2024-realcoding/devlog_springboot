d65e33afa3845a493ed42227cc72a8adc558aaa8
cyh1443 <126747535+cyh1443@users.noreply.github.com>
2024-06-10 20:42:24 +0900

    Update PostController.java

--- src/main/java/com/com/cnu/devlog_springboot/controller/PostController.java
+++ src/main/java/com/com/cnu/devlog_springboot/controller/PostController.java
@@ -3,6 +3,8 @@ package com.com.cnu.devlog_springboot.controller;
 import com.com.cnu.devlog_springboot.model.Post;
 import com.com.cnu.devlog_springboot.model.request.PostRequest;
 import com.com.cnu.devlog_springboot.service.PostService;
+import com.com.cnu.devlog_springboot.type.Tag;
+import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +19,8 @@ public class PostController {
 
     // GET /posts
     @GetMapping
-    public ResponseEntity<List<Post>> getPosts() {
-        return ResponseEntity.ok(postService.getPosts());
+    public ResponseEntity<List<Post>> getPosts(@RequestParam("tag") @Nullable Tag tag{
+        return ResponseEntity.ok(postService.getPosts(tag));
     }
 
     // GET /posts/{postId}
